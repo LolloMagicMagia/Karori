264e849df4ea638a9330246322e0eb1d5d320517
LolloMagicMagia <l.monti16@campus.unimib.it>
2023-01-29 00:49:46 +0100

    Fragment Olto con il Navigation.(Dati passati con un Bundle)

---
+++ app/src/main/java/com/example/karori/SearchClasses/SearchActivity.java
@@ -0,0 +1,32 @@
+package com.example.karori.SearchClasses;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.karori.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        /*Toolbar toolbar= findViewById(R.id.top_appbar);
+        setSupportActionBar(toolbar);*/
+
+
+
+
+
+
+    }
+
+
+
+}
